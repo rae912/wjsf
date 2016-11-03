e1636aa5eea905389bcb9ac586bf1d1d5f9aa5cf
WangCheng <wang.cheng@whaley.cn>
2016-11-03 14:39:07 +0800

    add two key word search and show

--- index.js
+++ index.js
@@ -41,9 +41,26 @@ app.post('/api/todos', function (req, res) {
 
 var request = require('request');
 
-var Data = { "query" : { "match_phrase" : { "description" : req.body.search } },
+var inputArr = req.body.search.split(" ");
+var str = '';
+for(var i = 0;i < inputArr.length-1; i++) {
+    console.log(inputArr[i]);
+    str += '{ "match_phrase" : {"description" : "'+inputArr[i]+'"}},';
+}
+str = str +'{ "match_phrase" : {"description" : "'+inputArr[inputArr.length-1]+'"}}';
+
+
+var Data = '{ "query" : {"bool" : {"must" :['+str+']}},'+
+    '"sort" : [{"timestamp" : { "order" : "desc" }}],'+
+    '"highlight" : {"pre_tags" : ["<span style=\'color:red\'>","<span style=\'color:red\'>"],'+
+    '"post_tags" : ["<\/span>", "<\/span>"],'+
+    '"fields" : {"description" : {}}}}';
+
+
+/*var jsonData = { "query" : { "match_phrase" : { "description" : req.body.search } },
   "sort" : [ { "timestamp" : { "order" : "desc" } } ]
-};
+};*/
+    var page = 0;
 
 if (isNaN(req.body.currentPage)) {
     var page = 0;
@@ -53,19 +70,20 @@ if (isNaN(req.body.currentPage)) {
 var host = 'http://172.16.17.98:9200/scrapyd/_search?size=10';
 
     console.log(page);
+    var jsonData = JSON.parse(Data);
 
 request({
     url: host + '&from='+ page,
     method: "POST",
     json: true,   // <--Very important!!!
-    body: Data
+    body: jsonData
 },     function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log('search:'+req.body.search);
             res.send(body);
             console.log("total:"+body.hits.total)
         } else {
-            console.log(error, response.statusCode, body)
+            console.log(error, body)
         }
     }
 );
